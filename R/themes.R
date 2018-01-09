6aa695cd6c98d684eb8d8f9c8806e9c3fe11467c
leonawicz <matt.rstat@gmail.com>
2018-01-09 10:50:04 -0700

    Tidy code.

--- R/themes.R
+++ R/themes.R
@@ -16,12 +16,21 @@
 #' @examples
 #' snapalettes()
 snapalettes <- function(palette = 1, n = NULL){
-  p <- switch(palette, 1 = c("#386cb0","#fdb462","#7fc97f","#ef3b2c","#662506","#a6cee3","#fb9a99","#984ea3","#ffff33"))
+  x <- .palettes
+  if(is.numeric(palette)){
+    p <- switch(palette, "1" = x[[1]])
+  } else {
+    p <- switch(palette, "q1" = x[[1]])
+  }
   if(is.null(n)) return(p)
   if(length(p) < n) stop(paste("The selected color palette contains fewer than", n, "colors."))
   if(n < length(p)) p <- p[1:n]
   p
 }
+
+.palettes <- list(
+  q1 = c("#386cb0", "#fdb462", "#7fc97f", "#ef3b2c", "#662506", "#a6cee3", "#fb9a99", "#984ea3", "#ffff33")
+)
 
 #' SNAP plot themes
 #'
@@ -52,8 +61,10 @@ snapalettes <- function(palette = 1, n = NULL){
 
 #' @export
 #' @rdname themes
-theme_snap <- function(base_size = 14, base_family = "", base_col = "black", base_fill = "white", grid_col = "#F0F0F0") {
-  (.theme_prep(base_size = base_size, base_family = base_family, base_col = base_col, base_fill = base_fill) +
+theme_snap <- function(base_size = 14, base_family = "", base_col = "black",
+                       base_fill = "white", grid_col = "#F0F0F0") {
+  .theme_prep(base_size = base_size, base_family = base_family,
+              base_col = base_col, base_fill = base_fill) +
      theme(plot.title = element_text(face = "bold", size = rel(1.2)),
            text = element_text(),
            panel.background = element_rect(colour = NA),
@@ -76,26 +87,26 @@ theme_snap <- function(base_size = 14, base_family = "", base_col = "black", bas
            plot.margin = unit(c(3, 1, 1, 1), "mm"),
            strip.background = element_rect(colour = "#f0f0f0", fill = "#f0f0f0"),
            strip.text = element_text(face = "bold")
-     ))
+     )
 }
 
 #' @export
 #' @rdname themes
-theme_snapdark <- function(base_size = 14, base_family = "", base_col = "white", base_fill = "#242424", grid_col = "#333333"){
-  theme_snap(base_size = base_size, base_family = base_family, base_col = base_col, base_fill = base_fill, grid_col = grid_col)
+theme_snapdark <- function(base_size = 14, base_family = "", base_col = "white",
+                           base_fill = "#242424", grid_col = "#333333"){
+  theme_snap(base_size = base_size, base_family = base_family,
+             base_col = base_col, base_fill = base_fill, grid_col = grid_col)
 }
 
 .theme_prep <- function(base_size = 12, base_family = "", base_col = "black", base_fill = "white"){
-  thm <- theme_gray(base_size = base_size, base_family = base_family)
-  for (i in names(thm)) {
-    if ("colour" %in% names(thm[[i]])) {
-      thm[[i]]["colour"] <- list(NULL)
-    }
-    if ("fill" %in% names(thm[[i]])) {
-      thm[[i]]["fill"] <- list(NULL)
-    }
+  x <- ggplot2::theme_gray(base_size = base_size, base_family = base_family)
+  for (i in names(x)) {
+    if ("colour" %in% names(x[[i]])) x[[i]]["colour"] <- list(NULL)
+    if ("fill" %in% names(x[[i]])) x[[i]]["fill"] <- list(NULL)
   }
-  thm + theme(panel.border = element_rect(fill = NA), legend.background = element_rect(colour = NA),
-              line = element_line(colour = base_col),
-              rect = element_rect(fill = base_fill, colour = base_col), text = element_text(colour = base_col))
+  x + theme(panel.border = element_rect(fill = NA),
+            legend.background = element_rect(colour = NA),
+            line = element_line(colour = base_col),
+            rect = element_rect(fill = base_fill, colour = base_col),
+            text = element_text(colour = base_col))
 }

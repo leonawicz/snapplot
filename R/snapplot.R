d912295dd839fd1ae20b9ca53be4527c1befbce5
leonawicz <matt.rstat@gmail.com>
2018-01-09 10:18:56 -0700

    Add package description and imports.

---
+++ R/snapplot.R
@@ -0,0 +1,14 @@
+#' snapplot: flexdashboards for SNAP data sets.
+#'
+#' Provides a suite of functions for graphing SNAP data sets.
+#'
+#' \code{snapplot} supports other SNAPverse packages, namely \code{snapstat}.
+#' It is recommended to use \code{snapstat} directly when working with SNAP data for graphing and analysis.
+#' \code{snapplot} is part of the SNAPverse collection of R packages.
+#'
+#' @docType package
+#' @name snapplot
+NULL
+
+#' @importFrom magrittr %>%
+NULL

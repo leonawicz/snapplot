8a8af4a34ebeb041d9f856fa78dd5ac2bbb8002a
leonawicz <matt.rstat@gmail.com>
2018-01-10 13:05:40 -0700

    Update description.

--- R/snapplot.R
+++ R/snapplot.R
@@ -1,10 +1,8 @@
-#' snapplot: flexdashboards for SNAP data sets.
+#' snapplot: functions for graphing SNAP data sets.
 #'
-#' Provides a suite of functions for graphing SNAP data sets.
-#'
-#' \code{snapplot} supports other SNAPverse packages, namely \code{snapstat}.
+#' \code{snapplot} a suite of functions for graphing and styling plots of publicly available climate science data sets from Scenarios Network for Alaska and Arctic Planning at the University of Alaska Fairbanks.
+#' \code{snapplot} is a memeber of the SNAPverse collection of R package and primarily serves as a supporting package to other SNAPverse packages, namely \code{snapstat}.
 #' It is recommended to use \code{snapstat} directly when working with SNAP data for graphing and analysis.
-#' \code{snapplot} is part of the SNAPverse collection of R packages.
 #'
 #' @docType package
 #' @name snapplot
